53c9d5aa2f3d8b702fc45aa7c0bf493f6213fb7d
Caroline Orengo <carol.orengo@gmail.com>
2025-02-18 17:03:36 -0300

    chalenge concluído

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let amigos [];
+
+const inputNome = document.getElementById('amigo');
+const ulListaAmigos = document.getElementById('listaAmigos');
+const ulResultado = document.getElementById('resultado');
+let nomes = [];
+
 
 function adicionarAmigo() {
-    let nome = document.querySelector('input').value
-    if(nome=="") {
-        alert"Por favor, insira um nome!"
-    else {
-        amigos.push(nome)
-        console.log(amigos)
-        exibirLista()
+    if(ulResultado.innerHTML !== '') {
+        ulResultado.innerHTML = '';
     }
-    limparCampo()
+
+    if(inputNome.value === '') {
+        alert('Por favor, insira um nome');
+    }else if(nomes.includes(inputNome.value)) {
+        alert('Amigo já adicionado');
+    }else {
+        nomes.push(inputNome.value)
     }
-    function limparCampo(){
-        nome=document.querySelector('input') 
-        nome.value=""         
-        
+    
+    atualizarLista();
+
+    inputNome.value = '';
+}
+
+
+function atualizarLista() {
+    ulListaAmigos.innerHTML = '';
+    for(let i = 0; i < nomes.length; i++) {
+        const li = document.createElement('li');
+        li.innerHTML = nomes[i];
+        ulListaAmigos.appendChild(li);
     }
+}
+
+
+function sortearAmigo() {
+    const indiceAleatorio = Math.floor((Math.random() * nomes.length)); 
+
+    if(nomes.length === 0) {
+        alert('lista vazia');
+    }else{
+        const li = document.createElement('li');
+        ulResultado.innerHTML = '';
+        li.innerHTML = `Nome Sorteado: ${nomes[indiceAleatorio]}`;
+        ulResultado.appendChild(li);
+    }
+
+    nomes = [];
+    atualizarLista();
 }
